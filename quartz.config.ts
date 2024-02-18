36534d1a40b7c0daf2cf42249c9cc763c560d0f5
Brenno B. Demarchi <130931374+brennodemarchi@users.noreply.github.com>
2024-02-18 13:05:40 -0300

    Quartz sync: Feb 18, 2024, 1:05 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       colors: {
         lightMode: {
           light: "#faf8f8",
-          lightgray: "#e5e5e5",
+          lightgray: "#c8c8c8",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
